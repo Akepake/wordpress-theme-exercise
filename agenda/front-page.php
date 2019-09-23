65cb8a14eefac4b82faf7347458a7e73f637063d
Aleksi Taivalantti <33146791+Akepake@users.noreply.github.com>
2019-09-23 13:13:32 +0300

    Add files via upload

---
+++ agenda/front-page.php
@@ -0,0 +1,28 @@
+<?php get_header();?>
+
+
+<div class="hero">
+    <div class="text-box"><h1>We do tables</h1><h2>Wood & Steel</h2></div>
+</div>
+<div class="container">
+<section class="testimonials">
+    <div class="row">
+    <div class="col-sm"><h3>philosophy</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ipsum voluptatibus dolorum, consequatur fuga voluptatem. Explicabo laboriosam delectus eligendi, inventore, facere perspiciatis nulla enim aliquam dolor atque voluptas sequi rerum.</p></div>
+    <div class="col-sm"><h3>sit & sat</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ipsum voluptatibus dolorum, consequatur fuga voluptatem. Explicabo laboriosam delectus eligendi, inventore, facere perspiciatis nulla enim aliquam dolor atque voluptas sequi rerum.</p></div>
+    <div class="col-sm"><h3>we do good</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ipsum voluptatibus dolorum, consequatur fuga voluptatem. Explicabo laboriosam delectus eligendi, inventore, facere perspiciatis nulla enim aliquam dolor atque voluptas sequi rerum.</p></div>
+    </div>
+</section>
+</div>
+<section class="info">
+    <div class="row">
+    <div class="col-8 living"></div>
+    <div class="col-4 sit"><h3>sit & sat</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ipsum voluptatibus dolorum, consequatur fuga voluptatem.</p></div>
+    </div>
+    <div class="row">
+    <div class="col-4 we_do"><h3>we do good</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ipsum voluptatibus dolorum, consequatur fuga voluptatem.</p></div>
+    <div class="col-8 bedroom"></div>
+    </div>
+</section>
+
+
+<?php get_footer();?>
